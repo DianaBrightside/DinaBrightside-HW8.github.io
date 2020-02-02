78d126775f6103b0a126dea40269eafe1baf88bc
DianaBrightside <31443423+DianaBrightside@users.noreply.github.com>
2020-02-02 18:18:02 +0200

    another trial taps

--- main.js
+++ main.js
@@ -1,9 +1,9 @@
 window.addEventListener('DOMContentLoaded', function () {
     let img = document.getElementById('corgi');
-    img.addEventListener('touchstart', function (e) {
+    img.addEventListener('pointerdown', function (e) {
         img.style.opacity = '50%';
     })
-    img.addEventListener('touchend', function () {
+    img.addEventListener('pointerup', function () {
         img.style.opacity = '100%';
     })
 });
