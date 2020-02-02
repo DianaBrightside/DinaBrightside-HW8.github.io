6ae424737ea346dfd33c6b00b8f2b41465d1028c
DianaBrightside <31443423+DianaBrightside@users.noreply.github.com>
2020-02-02 18:46:48 +0200

    handle added

--- main.js
+++ main.js
@@ -1,9 +1,9 @@
 window.addEventListener('DOMContentLoaded', function () {
     let img = document.getElementById('corgi');
-    img.addEventListener('pointerdown', function (e) {
+    img.addEventListener('touchstart', handleStart, function (e) {
         img.style.opacity = '50%';
     })
-    img.addEventListener('pointerup', function () {
+    img.addEventListener('touchend', handleEnd, function () {
         img.style.opacity = '100%';
     })
 });
