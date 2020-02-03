1d63a2f1f956339c8d832c1f41e4b65b515e6f94
DianaBrightside <31443423+DianaBrightside@users.noreply.github.com>
2020-02-03 15:47:40 +0200

    preventDefault and useCapture added

--- main.js
+++ main.js
@@ -1,9 +1,11 @@
 window.addEventListener('DOMContentLoaded', function () {
     let img = document.getElementById('corgi');
-    img.addEventListener('touchstart', function (event) {
-        event.target.style.opacity = '50%';
-    })
-    img.addEventListener('touchend', function (event) {
-        event.target.style.opacity = '100%';
-    })
+    img.addEventListener('touchstart', function (ev) {
+        ev.preventDefault();
+        ev.target.style.opacity = '50%';
+    }, false)
+    img.addEventListener('touchend', function (ev) {
+        ev.preventDefault();
+        ev.target.style.opacity = '100%';
+    }, false)
 });
